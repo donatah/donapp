b74dd2d13477a5fd60636aecf04da29dd86a6a3f
AdaNamanya0 <namanyaaada@gmail.com>
2020-02-18 19:12:36 +0300

    hello world

---
+++ app/src/main/java/com/example/donapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.donapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view){
+        EditText message=(EditText)findViewById(R.id.message);
+        Toast.makeText(this,"send message"+message.getText().toString(),
+                Toast.LENGTH_SHORT).show();
+Intent intent=new Intent(this,DisplayMessageActivity.class);
+startActivity(intent);
+message.setText("");
+
+}
+
+}
